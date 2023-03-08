dcfb48f4312ceb022a0edaa5db026e50c9d790e4
vika <valenciavika1@gmail.com>
2023-03-08 14:53:51 +0700

    second changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,12 +8,12 @@
         <title>Profile</title>
     </head>
     <body>
-        <div class ="navbar flex items-center justify-between mx-10">
+        <div class ="navbar flex items-center justify-between sm:mx-10 mx-5">
             <h2 class = "text2 font-bold font-serif text-slate-900">PROFILE</h2>
-            <div class = "button py-5 font-bold mx font-serif text-slate-900">
-                <a class = "textBut mr-10" href="">Jasons</a>
-                <a class = "textBut mr-10" href="">Matthew</a>
-                <a class = "textBut mr-10" href="">Vieren</a>
+            <div class = "button py-5 font-bold font-serif text-slate-900">
+                <a class = "textBut md:mr-10 mr-5" href="">Jasons</a>
+                <a class = "textBut md:mr-10 mr-5" href="">Matthew</a>
+                <a class = "textBut md:mr-10 mr-5" href="">Vieren</a>
                 <a class = "textBut" href="">Vika</a>
             </div>
 
@@ -30,63 +30,80 @@
             </div>
         </div>
         <div class="about_me_section">
-            <div class="bg-slate-700 py-24">
+            <div class="bg-slate-700 sm:py-24 py-10">
                 <div class="mx-auto max-w-7xl px-6 lg:px-8">
                     <div class="mx-auto">
-                    <h2 class="text2 font-bold font-serif text-white text-center">About Me</h2>
-                    <p class="text3 font-serif font-light mt-6 text-lg text-white text-center">You can call me Jeje, I'm from Indonesia living in Jakarta. Dedicated Computer Science student with the ability to work well with others. Interested both in Front-end and Back-end development. Motivated to learn something new when solving problems.  Able to self-manage during independent projects and collaborate as part of a team.</p>
+                        <h2 class="text2 font-bold font-serif text-white text-center">About Me</h2>
+                        <p class="text3 font-serif font-light py-6 text-lg text-white text-center">You can call me Jeje, I'm from Indonesia living in Jakarta. Dedicated Computer Science student with the ability to work well with others. Interested both in Front-end and Back-end development. Motivated to learn something new when solving problems.  Able to self-manage during independent projects and collaborate as part of a team.</p>
                     </div>
-                    <div class="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
-                    </div>
+
                 </div>
             </div>
         </div>
         <div class="profile_section bg-slate-500">
             <div class="profile_section_content">
-                <div class="overflow-hidden shadow flex p-10">
-                    <div class="detail text3">
-                        <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 rounded-t-2xl">
-                            <dt class="font-serif font-medium text-slate-500">Full name</dt>
-                            <dd class="mt-1 font-serif text-slate-900 sm:col-span-2 sm:mt-0">Jasons Januard Bongtari</dd>
+                <div class="overflow-hidden shadow flex sm:p-10 p-5">
+                    <div class="detail text3 items-center">
+                        <div class="bg-gray-50 px-4 sm:py-5 py-3 grid grid-cols-2 sm:gap-4 sm:px-6 rounded-t-2xl">
+                            <dt class="font-serif font-medium text-slate-500 flex items-center">Full name</dt>
+                            <dd class="font-serif text-slate-900 sm:mt-0 flex items-center">Jasons Januard Bongtari</dd>
                         </div>
-                        <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                            <dt class="font-serif font-medium text-slate-500">Gender</dt>
-                            <dd class="mt-1 font-serif text-slate-900 sm:col-span-2 sm:mt-0">Male</dd>
+                        <div class="bg-white px-4 sm:py-5 py-3 grid grid-cols-2 sm:gap-4 sm:px-6">
+                            <dt class="font-serif font-medium text-slate-500 flex items-center">Gender</dt>
+                            <dd class="font-serif text-slate-900 sm:mt-0 flex items-center">Male</dd>
                         </div>
-                        <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                            <dt class="font-serif font-medium text-slate-500">Date of birth</dt>
-                            <dd class="mt-1 font-serif text-slate-900 sm:col-span-2 sm:mt-0">16 January 2002</dd>
+                        <div class="bg-gray-50 px-4 sm:py-5 py-3 grid grid-cols-2 sm:gap-4 sm:px-6">
+                            <dt class="font-serif font-medium text-slate-500 flex items-center">Date of birth</dt>
+                            <dd class="font-serif text-slate-900 sm:mt-0 flex items-center">16 January 2002</dd>
                         </div>
-                        <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                            <dt class="font-serif font-medium text-slate-500">Place of birth</dt>
-                            <dd class="mt-1 font-serif text-slate-900 sm:col-span-2 sm:mt-0">Jakarta, Indonesia</dd>
+                        <div class="bg-white px-4 sm:py-5 py-3 grid grid-cols-2 sm:gap-4 sm:px-6">
+                            <dt class="font-serif font-medium text-slate-500 flex items-center">Place of birth</dt>
+                            <dd class="font-serif text-slate-900 sm:mt-0 flex items-center">Jakarta, Indonesia</dd>
                         </div>
-                        <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                            <dt class="font-serif font-medium text-slate-500">Email address</dt>
-                            <dd class="mt-1 font-serif text-slate-900 sm:col-span-2 sm:mt-0">jejebe112@gmail.com</dd>
+                        <div class="bg-gray-50 px-4 sm:py-5 py-3 grid grid-cols-2 sm:gap-4 sm:px-6">
+                            <dt class="font-serif font-medium text-slate-500 flex items-center">Email address</dt>
+                            <dd class="font-serif text-slate-900 sm:mt-0 flex items-center">jejebe112@gmail.com</dd>
                         </div>
-                        <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 rounded-b-2xl">
-                            <dt class="font-serif font-medium text-slate-500">Phone number</dt>
-                            <dd class="mt-1 font-serif text-slate-900 sm:col-span-2 sm:mt-0">+62 818 0123 4567</dd>
+                        <div class="bg-white px-4 sm:py-5 py-3 grid grid-cols-2 sm:gap-4 sm:px-6 rounded-b-2xl">
+                            <dt class="font-serif font-medium text-slate-500 flex items-center">Phone number</dt>
+                            <dd class="font-serif text-slate-900 sm:mt-0 flex items-center">+62 818 0123 4567</dd>
                         </div>
                     </div>
-                    <div class="text3 skills_hobbies pl-10 h-full self-center">
-                        <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 rounded-t-2xl">
-                            <dt class="font-serif font-medium text-slate-500">Soft Skills</dt>
-                            <dd class="mt-1 font-serif text-slate-900 sm:col-span-2 sm:mt-0">
-                                <div class="flex justify-around"><p>Team work</p><p>Time management</p><p>Perseverence</p></div>
+                    <div class="text3 skills_hobbies sm:pl-10 h-full pl-5 self-center">
+                        <div class="bg-gray-50 px-4 sm:py-5 py-3 grid grid-cols-4 sm:gap-4 sm:px-6 rounded-t-2xl">
+                            <dt class="font-serif font-medium text-slate-500 flex items-center">Soft Skills</dt>
+                            <dd class="font-serif text-slate-900 sm:mt-0 flex items-center">
+                                <p>Team work</p>
+                            </dd>
+                            <dd class="font-serif text-slate-900 sm:mt-0 flex items-center">
+                                <p>Time management</p>
+                            </dd>
+                            <dd class="font-serif text-slate-900 sm:mt-0 flex items-center">
+                                <p>Perseverence</p>
                             </dd>
                         </div>
-                        <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                            <dt class="font-serif font-medium text-slate-500">Hard Skills</dt>
-                            <dd class="mt-1 font-serif text-slate-900 sm:col-span-2 sm:mt-0">
-                                <div class="flex justify-around"><p>HTML</p><p>Javascript</p><p>CSS</p></div>
+                        <div class="bg-white px-4 sm:py-5 py-3 grid grid-cols-4 sm:gap-4 sm:px-6">
+                            <dt class="font-serif font-medium text-slate-500 flex items-center">Hard Skills</dt>
+                            <dd class="font-serif text-slate-900 sm:mt-0 flex items-center">
+                                <p>HTML</p>
+                            </dd>
+                            <dd class="font-serif text-slate-900 sm:mt-0 flex items-center">
+                                <p>Javascript</p>
+                            </dd>
+                            <dd class="font-serif text-slate-900 sm:mt-0 flex items-center">
+                                <p>CSS</p>
                             </dd>
                         </div>
-                        <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 rounded-b-2xl">
-                            <dt class="font-serif font-medium text-slate-500">Hobbies</dt>
-                            <dd class="mt-1 font-serif text-slate-900 sm:col-span-2 sm:mt-0">
-                                <div class="flex justify-around"><p>Tennis</p><p>Swim</p><p>Volleyball</p></div>
+                        <div class="bg-gray-50 px-4 sm:py-5 py-3 grid grid-cols-4 sm:gap-4 sm:px-6 rounded-b-2xl">
+                            <dt class="font-serif font-medium text-slate-500 flex items-center">Hobbies</dt>
+                            <dd class="font-serif text-slate-900 sm:mt-0 flex items-center">
+                                <p>Tennis</p>                              
+                            </dd>
+                            <dd class="font-serif text-slate-900 sm:mt-0 flex items-center">
+                                <p>Swim</p> 
+                            </dd>
+                            <dd class="font-serif text-slate-900 sm:mt-0 flex items-center">
+                                <p>Volleyball</p>
                             </dd>
                         </div>
                     </div>
@@ -99,50 +116,47 @@
                 <div class="education text3 overflow-hidden bg-white shadow rounded-2xl">
                     <div class="border-t">
                         <dl>
-                            <div class="education bg-white px-4 py-5 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-6 text-slate-900">
+                            <div class="education bg-white px-4 sm:py-5 py-3 grid grid-cols-2 gap-4 sm:px-6 text-slate-900">
                                 <dt class=" font-serif"><p>Binus University</p>
                                 <p>Computer Science & Statistics</p></dt>
-                                <dd class="mt-1 font-serif flex justify-end">2020 - 2024</dd>
+                                <dd class="font-serif flex justify-end items-center">2020 - 2024</dd>
                             </div>
                         </dl>
                     </div>
     
-                    <div class="px-4 py-5 sm:px-6">
+                    <div class="px-4 sm:py-5 py-3 sm:px-6">
                         <h3 class="font-semibold font-serif leading-6 text-slate-900 text-lg text3">Favourite Course</h3>
                     </div>
                     <div class="border-t">
                         <dl>
-                            <div class="education bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-6">
-                                <dt class="font-serif font-medium text-slate-500">2020, Odd Semester</dt>
-                                <dd class="mt-1 font-serif text-slate-900 ">Algorithm and Programming</dd>
+                            <div class="education bg-gray-50 px-4 sm:py-5 py-3 grid grid-cols-2 gap-4 sm:px-6">
+                                <dt class="font-serif font-medium text-slate-500 flex items-center">2020, Odd Semester</dt>
+                                <dd class="font-serif text-slate-900 flex item-center">Algorithm and Programming</dd>
                             </div>
-                            <div class="education bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-6">
-                                <dt class="font-serif font-medium text-slate-500">2020, Even Semester</dt>
-                                <dd class="mt-1 font-serif text-slate-900 ">Data Structure</dd>
+                            <div class="education bg-gray-50 px-4 sm:py-5 py-3 grid grid-cols-2 gap-4 sm:px-6">
+                                <dt class="font-serif font-medium text-slate-500 flex items-center">2020, Even Semester</dt>
+                                <dd class="font-serif text-slate-900 flex item-center ">Data Structure</dd>
                             </div>
-                            <div class="education bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-6">
-                            <dt class="font-serif font-medium text-slate-500">2021, Odd Semester</dt>
-                                <dd class="mt-1 font-serif text-slate-900 ">Object Oriented Programming</dd>
+                            <div class="education bg-gray-50 px-4 sm:py-5 py-3 grid grid-cols-2 gap-4 sm:px-6">
+                            <dt class="font-serif font-medium text-slate-500 flex items-center">2021, Odd Semester</dt>
+                                <dd class="font-serif text-slate-900 flex item-center ">Object Oriented Programming</dd>
                             </div>
-                            <div class="education bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-6">
-                            <dt class="font-serif font-medium text-slate-500">2021, Even Semester</dt>
-                                <dd class="mt-1 font-serif text-slate-900 ">Database</dd>
+                            <div class="education bg-gray-50 px-4 sm:py-5 py-3 grid grid-cols-2 gap-4 sm:px-6">
+                            <dt class="font-serif font-medium text-slate-500 flex items-center">2021, Even Semester</dt>
+                                <dd class="font-serif text-slate-900 flex item-center ">Database</dd>
                             </div>
-                            <div class="education bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-6">
-                            <dt class="font-serif font-medium text-slate-500">2022, Odd Semester</dt>
-                                <dd class="mt-1 font-serif text-slate-900 ">Deep Learning</dd>
+                            <div class="education bg-gray-50 px-4 sm:py-5 py-3 grid grid-cols-2 gap-4 sm:px-6">
+                            <dt class="font-serif font-medium text-slate-500 flex items-center">2022, Odd Semester</dt>
+                                <dd class="font-serif text-slate-900 flex item-center ">Deep Learning</dd>
                             </div>
-                            <div class="education bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-6">
-                            <dt class="font-serif font-medium text-slate-500">2022, Even Semester</dt>
-                                <dd class="mt-1 font-serif text-slate-900">Web Programming</dd>
+                            <div class="education bg-gray-50 px-4 sm:py-5 py-3 grid grid-cols-2 gap-4 sm:px-6">
+                            <dt class="font-serif font-medium text-slate-500 flex items-center">2022, Even Semester</dt>
+                                <dd class="font-serif text-slate-900 flex item-center">Web Programming</dd>
                             </div>
                         </dl>
                     </div>
                 </div>
             </div>
         </div>
-
-        
-
     </body>
 </html>
