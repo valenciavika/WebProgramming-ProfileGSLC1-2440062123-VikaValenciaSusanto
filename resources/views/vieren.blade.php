f1d5276265fb8fa5ac4ba709130d4531f3f48706
vierenC <vierencristian16@gmail.com>
2023-03-09 17:29:21 +0700

    fixing navbar href

--- resources/views/vieren.blade.php
+++ resources/views/vieren.blade.php
@@ -16,7 +16,7 @@
 
             <div class = "button font-bold font-serif text-slate-900 flex">
                 <a class = "textBut md:mr-10 mr-5" href="\">Jasons</a>
-                <a class = "textBut md:mr-10 mr-5" href="">Matthew</a>
+                <a class = "textBut md:mr-10 mr-5" href="/matt">Matthew</a>
                 <a class = "textBut md:mr-10 mr-5" href="/vieren">Vieren</a>
                 <a class = "textBut" href="/vika">Vika</a>
             </div>
